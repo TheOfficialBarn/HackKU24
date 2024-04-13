1656b1e69fa2d1b3a894a6417258e3dd9400498a
Aiden Dorantes Barnard <apbarnard2004@icloud.com>
2024-04-13 13:15:40 -0500

    Prettified the home page

--- bound2clean/lib/pages/HomePage.dart
+++ bound2clean/lib/pages/HomePage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({super.key});
 
   @override
   _HomePageState createState() => _HomePageState();
@@ -12,15 +12,36 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return const Scaffold(
       backgroundColor: Color.fromARGB(255, 0, 20, 45),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-        Text('Current Hygiene Streak:', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
-        Text('24 Days!', style: TextStyle(color: Colors.red)),
-          ],
-        ),
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                'Current Hygiene Streak',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 32, // Increase font size for grand header
+                  decoration: TextDecoration.underline,
+                  decorationColor: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Text(
+              '24 Days',
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 60, // Increase font size
+                fontWeight: FontWeight.w900
+              ),
+            ),
+          ),
+        ],
       ),
-      );
+    );
   }
 }
