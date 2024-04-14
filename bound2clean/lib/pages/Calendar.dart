0f9c5a7e1b35c55a918c327a2f60f72f460baa58
flec1115 <balandranf11@gmail.com>
2024-04-14 04:39:14 -0500

    calendar stuff

--- bound2clean/lib/pages/Calendar.dart
+++ bound2clean/lib/pages/Calendar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class Calendar extends StatefulWidget {
   const Calendar({Key? key}) : super(key: key);
@@ -8,13 +9,38 @@ class Calendar extends StatefulWidget {
 }
 
 class _CalendarState extends State<Calendar> {
+  DateTime today = DateTime.now();
+  void _onDaySelected(DateTime day, DateTime focusedDay) {
+    setState(() {
+      today = day;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue[200],
-      body: const Center(
-        child: Text('Hiiiiiii World!'),
+      body: Center(
+        child: content(), // Call the content method here
       ),
     );
   }
+
+  Widget content() {
+    return Column(
+      children: [
+        Container(
+          child: TableCalendar(
+            selectedDayPredicate: (day)=>isSameDay(day, today),
+            focusedDay: today,
+            firstDay: DateTime.utc(2010, 10, 16),
+            lastDay: DateTime.utc(2030, 3, 14),
+            onDaySelected: _onDaySelected, 
+          ),
+        ),
+      ],
+    );
+  }
 }
+
+  
